0f32c4f67af02c961c80f8f2cdeb853d1abe31f6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-09 13:33:06 -0500

    🚧  Add HTM structure for mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        veryDarkBlue: "hsl(243, 87%, 12%)",
+        desaturatedBlue: "hsl(238, 22%, 44%)",
+        brightBlue: "hsl(224, 93%, 58%)",
+        moderateCyan: "hsl(170, 45%, 43%)",
+        lightGrayishBlue: "hsl(240, 75%, 98%)",
+        lightGray: "hsl(0, 0%, 75%)",
+      },
+    },
   },
   plugins: [],
-}
+};
